b41f0a5ff870adad0309e635e3b99df33e4a634d
ishadu4 <israelfrancis472@gmail.com>
2023-03-20 05:31:08 -0700

    cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+Â#include <stdio.h>
+
+/**
+* main - print the string in the function
+*
+* Description: using the main function
+* this progra prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzleÂ");
+	return (0);
+}
