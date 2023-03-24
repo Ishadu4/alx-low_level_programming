e79a57c115f84c5352a1790c61c185b6ae2d6520
ishadu4 <israelfrancis472@gmail.com>
2023-03-24 03:20:21 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+ 0-main.c
+#include "main.h"                       
+
+/**
+* main - test function that prints if integer is positive ir negative       
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative()
+
+return (0);
